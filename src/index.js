77c417dc1f0bcd7ecc3dc89db271a4e762d59f8f
unknown <impratsbanerjee@gmail.com>
2021-05-11 00:09:41 +0530

    changed some text

--- src/index.js
+++ src/index.js
@@ -25,39 +25,27 @@ window.addEventListener("DOMContentLoaded", () => {
     delay += 0.2;
   });
 });
+
+function addTransition(container, mainContainer = false) {
+  Array.from(container.children).forEach((section, i) => {
+    if (mainContainer && i === 0) return;
+    let observer = new IntersectionObserver(
+      (e) => {
+        if (e[0].isIntersecting) {
+          transitionate(e[0].target);
+        }
+      },
+      { threshold: mainContainer ? 0 : 0.5 }
+    );
+    observer.observe(section);
+  });
+}
+
+addTransition(select(".main-container"), true);
+addTransition(select(".featrued-projects-container"));
+// addTransition(select(".other-projects-container"));
+
 select(".hambuger-icon").addEventListener("click", () => {
   select(".hambuger-icon").classList.toggle("show_nav");
   select(".drawer-menu").classList.toggle("show_drawer-menu");
-});
-
-[...select(".main-container").children].forEach((section, i) => {
-  if (i === 0) return;
-  let observer = new IntersectionObserver((e) => {
-    if (e[0].isIntersecting) transitionate(e[0].target);
-  });
-  observer.observe(section);
-});
-
-[...select(".featrued-projects-container").children].forEach((section) => {
-  let observer = new IntersectionObserver(
-    (e) => {
-      if (e[0].isIntersecting) {
-        transitionate(e[0].target);
-      }
-    },
-    { threshold: 0.5 }
-  );
-  observer.observe(section);
-});
-
-[...select(".other-projects-container").children].forEach((section) => {
-  let observer = new IntersectionObserver(
-    (e) => {
-      if (e[0].isIntersecting) {
-        transitionate(e[0].target);
-      }
-    },
-    { threshold: 0.5 }
-  );
-  observer.observe(section);
 });
