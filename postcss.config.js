75b33bc3b597aa37fbf65e57789400da6d432511
unknown <impratsbanerjee@gmail.com>
2021-05-12 09:44:01 +0530

    removed unnecessary packages initially installed and modified some css for better responsiveness

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require("autoprefixer"), require("postcss-inline-svg")],
+  plugins: [
+    require("postcss-preset-env"),
+    require("autoprefixer"),
+    require("postcss-inline-svg"),
+  ],
 };
